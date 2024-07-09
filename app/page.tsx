dfbc2a19c03dbabbc02a08e1f727b3bdf0e588a5
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-07-09 09:49:42 +0530

    Added images and other assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
     return (
-        <div>
-            <h1 className="text-3xl underline text-white">Home</h1>
-            <Button>Click me</Button>
+        <div className="h-screen flex max-h-screen">
+            <section className="remove-scrollbar container my-auto">
+                <div className='sub-container max-w-[496px]'>
+                    <Image src={'/assets/images/logo/logo.png'} alt="logo" width={200} height={200} />
+                </div>
+            </section>
         </div>
     );
 }
